86e1a41726df2dd8d96d7f2fa1f4a9650607daa4
collinhundley <collinhundley@gmail.com>
2016-08-02 18:52:49 -0600

    Update dependency to collinhundley/cmysql

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "MySQL",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/cmysql.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/collinhundley/cmysql.git", majorVersion: 0, minor: 2)
     ]
 )
