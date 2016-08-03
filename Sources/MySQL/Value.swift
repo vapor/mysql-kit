d099306b34fe93068e28168528c225a69de48017
collinhundley <collinhundley@gmail.com>
2016-08-02 19:10:18 -0600

    Add type accessors to

--- Sources/MySQL/Value.swift
+++ Sources/MySQL/Value.swift
@@ -8,4 +8,50 @@ public enum Value {
     case uint(UInt)
     case double(Double)
     case null
+    
+    public var string: String? {
+        switch self {
+        case let .string(val):
+            return val
+        default:
+            return nil
+        }
+    }
+    
+    public var int: Int? {
+        switch self {
+        case let .int(val):
+            return val
+        default:
+            return nil
+        }
+    }
+    
+    public var uint: UInt? {
+        switch self {
+        case let .uint(val):
+            return val
+        default:
+            return nil
+        }
+    }
+    
+    public var double: Double? {
+        switch self {
+        case let .double(val):
+            return val
+        default:
+            return nil
+        }
+    }
+    
+    public var isNull: Bool {
+        switch self {
+        case .null:
+            return true
+        default:
+            return false
+        }
+    }
+    
 }
