63028cbbc882454bf683446ffb6ebd723e915499
Collin Hundley <collinhundley@gmail.com>
2016-08-02 22:32:02 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "MySQL",
     dependencies: [
-        .Package(url: "https://github.com/collinhundley/cmysql.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/collinhundley/CMariaDB.git", majorVersion: 0, minor: 1)
     ]
 )
