931a44ad43c3ca5f122bb808c6cb9224feab43fe
Brett R. Toomey <brettcwx@gmail.com>
2016-08-05 12:19:34 -0700

    Great rename

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@ let package = Package(
     name: "MySQL",
     dependencies: [
         // Module map for `libmysql`
-        .Package(url: "https://github.com/qutheory/cmysql.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/vapor/cmysql.git", majorVersion: 0, minor: 2),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/qutheory/node.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 0, minor: 4),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks
-        .Package(url: "https://github.com/qutheory/core.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 0, minor: 3),
 
         // JSON parsing and serialization for storing arrays and objects in MySQL
-        .Package(url: "https://github.com/qutheory/json.git", majorVersion: 0, minor: 3)
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 0, minor: 4)
     ]
 )
