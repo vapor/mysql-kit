637f7f688fb418a25be54243ae04486d892b65d5
Tanner Nelson <me@tanner.xyz>
2016-09-10 23:32:20 -0400

    global error enum

--- Sources/MySQL/Error.swift
+++ Sources/MySQL/Error.swift
@@ -1,9 +1,17 @@
-//
-//  Error.swift
-//  MySQL
-//
-//  Created by Tanner Nelson on 9/10/16.
-//
-//
+/**
+    A list of all Error messages that
+    can be thrown from calls to `Database`.
 
-import Foundation
+    All Error objects contain a String which
+    contains MySQL's last error message.
+*/
+public enum Error: Swift.Error {
+    case serverInit
+    case connection(String)
+    case inputBind(String)
+    case outputBind(String)
+    case fetchFields(String)
+    case prepare(String)
+    case statement(String)
+    case execute(String)
+}
