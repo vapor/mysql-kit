9d5697bec8b18b79c8b0cacb39df0b35c4da22f6
Tanner Nelson <me@tanner.xyz>
2016-09-10 23:32:15 -0400

    simplify database

--- Sources/MySQL/Database.swift
+++ Sources/MySQL/Database.swift
@@ -15,24 +15,6 @@ import Core
 */
 public final class Database {
     /**
-        A list of all Error messages that
-        can be thrown from calls to `Database`.
-     
-        All Error objects contain a String which
-        contains MySQL's last error message.
-    */
-    public enum Error: Swift.Error {
-        case serverInit
-        case connection(String)
-        case inputBind(String)
-        case outputBind(String)
-        case fetchFields(String)
-        case prepare(String)
-        case statement(String)
-        case execute(String)
-    }
-
-    /**
         Attempts to establish a connection to a MySQL database
         engine running on host.
      
@@ -118,99 +100,7 @@ public final class Database {
             connection = try makeConnection()
         }
 
-        // Create a pointer to the statement
-        // This should only fail if memory is limited.
-        guard let statement = mysql_stmt_init(connection.cConnection) else {
-            throw Error.statement(connection.error)
-        }
-        defer {
-            mysql_stmt_close(statement)
-        }
-
-        // Prepares the created statement
-        // This parses `?` in the query and
-        // prepares them to attach parameterized bindings.
-        guard mysql_stmt_prepare(statement, query, strlen(query)) == 0 else {
-            throw Error.prepare(connection.error)
-        }
-
-        // Transforms the `[Value]` array into bindings
-        // and applies those bindings to the statement.
-        let inputBinds = try Binds(values)
-        guard mysql_stmt_bind_param(statement, inputBinds.cBinds) == 0 else {
-            throw Error.inputBind(connection.error)
-        }
-
-        // Fetches metadata from the statement which has
-        // not yet run.
-        if let metadata = mysql_stmt_result_metadata(statement) {
-            defer {
-                mysql_free_result(metadata)
-            }
-
-            // Parse the fields (columns) that will be returned
-            // by this statement.
-            let fields: Fields
-            do {
-                fields = try Fields(metadata)
-            } catch {
-                throw Error.fetchFields(connection.error)
-            }
-
-            // Use the fields data to create output bindings.
-            // These act as buffers for the data that will
-            // be returned when the statement is executed.
-            let outputBinds = Binds(fields)
-
-            // Bind the output bindings to the statement.
-            guard mysql_stmt_bind_result(statement, outputBinds.cBinds) == 0 else {
-                throw Error.outputBind(connection.error)
-            }
-
-            // Execute the statement!
-            // The data is ready to be fetched when this completes.
-            guard mysql_stmt_execute(statement) == 0 else {
-                throw Error.execute(connection.error)
-            }
-
-            var results: [[String: Node]] = []
-
-            // Iterate over all of the rows that are returned.
-            // `mysql_stmt_fetch` will continue to return `0`
-            // as long as there are rows to be fetched.
-            while mysql_stmt_fetch(statement) == 0 {
-                var parsed: [String: Node] = [:]
-
-                // For each row, loop over all of the fields expected.
-                for (i, field) in fields.fields.enumerated() {
-
-                    // For each field, grab the data from
-                    // the output binding buffer and add
-                    // it to the parsed results.
-                    let output = outputBinds[i]
-                    parsed[field.name] = output.value
-
-                }
-
-                results.append(parsed)
-
-                // reset the bindings onto the statement to 
-                // signal that they may be reused as buffers
-                // for the next row fetch.
-                guard mysql_stmt_bind_result(statement, outputBinds.cBinds) == 0 else {
-                    throw Error.outputBind(connection.error)
-                }
-            }
-
-            return results
-        } else {
-            // no data is expected to return from 
-            // this query, simply execute it.
-            guard mysql_stmt_execute(statement) == 0 else {
-                throw Error.execute(connection.error)
-            }
-            return []
-        }
+        return try connection.execute(query, values)
     }
 
 
