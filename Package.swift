0fd08de2c558230b4b73633c5c68ec95290ebfeb
Tanner Nelson <me@tanner.xyz>
2017-03-27 17:17:31 +0100

    beta

--- Package.swift
+++ Package.swift
@@ -1,18 +1,20 @@
 import PackageDescription
 
+let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
+
 let package = Package(
     name: "MySQL",
     dependencies: [
         // Module map for `libmysql`
-        .Package(url: "https://github.com/vapor/cmysql.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/cmysql.git", beta),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/node.git", beta),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/core.git", beta),
 
         // JSON parsing and serialization for storing arrays and objects in MySQL
-        .Package(url: "https://github.com/vapor/json.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"]))
+        .Package(url: "https://github.com/vapor/json.git", beta)
     ]
 )
