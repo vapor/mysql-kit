2712d452f0a37716055525896fa48234f1bb964d
Joannis Orlandos <joannis@orlandos.nl>
2017-08-20 13:21:18 +0200

    No!

--- Package.swift
+++ Package.swift
@@ -3,16 +3,7 @@ import PackageDescription
 let package = Package(
     name: "MySQL",
     dependencies: [
-        // Module map for `libmysql`
-        .Package(url: "https://github.com/vapor/cmysql.git", majorVersion: 2),
-
-        // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
-
         // Core extensions, type-aliases, and functions that facilitate common tasks
         .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
-
-        // JSON parsing and serialization for storing arrays and objects in MySQL
-        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2),
     ]
 )
