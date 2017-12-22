17369a9e5781c8fdb9ab1849cebfb6fe999c8b39
Joannis Orlandos <joannis@orlandos.nl>
2017-12-22 12:21:37 +0100

    reverse parameter binding parsing

--- Sources/MySQL/Query/Connection+PreparedStatement.swift
+++ Sources/MySQL/Query/Connection+PreparedStatement.swift
@@ -13,60 +13,61 @@ extension MySQLConnection {
         var statement: PreparedStatement?
         _ = self.parser.drain { parser in
             parser.request()
-        }.output { packet in
-            if packet.payload.first == 0xfe {
-                // EOF, end of columns
-                if
-                    let statement = statement,
-                    statement.parameterCount == statement.parameters.count,
-                    packet.payload.count == 5,
-                    packet.payload.first == 0xfe
-                {
-                    promise.complete(statement)
-                }
-                
-                // Ignore `0xfe` payloads, since we skip past those in the above `do {} catch {}`
-                self.parser.request()
-                return
-            }
-            
-            if let statement = statement {
-                // Continue processing the statement preparation
-                if statement.columns.count < statement.columnCount {
-                    statement.columns.append(try packet.parseFieldDefinition())
-                } else if statement.parameters.count < statement.parameterCount {
-                    statement.parameters.append(try packet.parseFieldDefinition())
-                }
-                
-                self.parser.request()
-            } else {
-                // Statement preparation details not yet read
-                guard packet.payload.count == 12, packet.payload.first == 0x00 else {
-                    throw MySQLError(packet: packet)
-                }
-                
-                var parser = Parser(packet: packet, position: 1)
-                
-                let statementID = try parser.parseUInt32()
-                let columnCount = try parser.parseUInt16()
-                let parameterCount = try parser.parseUInt16()
-                
-                let preparedStatement = PreparedStatement(
-                    statementID: statementID,
-                    columnCount: columnCount,
-                    connection: self,
-                    parameterCount: parameterCount
-                )
-                
-                if columnCount == 0 && parameterCount == 0 {
-                    promise.complete(preparedStatement)
+            }.output { packet in
+                if packet.payload.first == 0xfe {
+                    // EOF, end of columns
+                    if
+                        let statement = statement,
+                        statement.parameterCount == statement.parameters.count,
+                        statement.columnCount == statement.columns.count,
+                        packet.payload.count == 5,
+                        packet.payload.first == 0xfe
+                    {
+                        promise.complete(statement)
+                    }
+                    
+                    // Ignore `0xfe` payloads, since we skip past those in the above `do {} catch {}`
+                    self.parser.request()
                     return
                 }
                 
-                statement = preparedStatement
-                self.parser.request()
-            }
-        }.catch(onError: promise.fail)
+                if let statement = statement {
+                    // Continue processing the statement preparation
+                    if statement.parameters.count < statement.parameterCount {
+                        statement.parameters.append(try packet.parseFieldDefinition())
+                    } else if statement.columns.count < statement.columnCount {
+                        statement.columns.append(try packet.parseFieldDefinition())
+                    }
+                    
+                    self.parser.request()
+                } else {
+                    // Statement preparation details not yet read
+                    guard packet.payload.count == 12, packet.payload.first == 0x00 else {
+                        throw MySQLError(packet: packet)
+                    }
+                    
+                    var parser = Parser(packet: packet, position: 1)
+                    
+                    let statementID = try parser.parseUInt32()
+                    let columnCount = try parser.parseUInt16()
+                    let parameterCount = try parser.parseUInt16()
+                    
+                    let preparedStatement = PreparedStatement(
+                        statementID: statementID,
+                        columnCount: columnCount,
+                        connection: self,
+                        parameterCount: parameterCount
+                    )
+                    
+                    if columnCount == 0 && parameterCount == 0 {
+                        promise.complete(preparedStatement)
+                        return
+                    }
+                    
+                    statement = preparedStatement
+                    self.parser.request()
+                }
+            }.catch(onError: promise.fail)
         
         do {
             try self.prepare(query: query.queryString)
@@ -94,14 +95,14 @@ extension MySQLConnection {
         
         _ = self.parser.drain { connection in
             connection.request()
-        }.output { packet in
-            guard packet.payload.first == 0x00 else {
-                promise.fail(MySQLError(packet: packet))
-                return
-            }
-            
-            promise.complete()
-        }.catch(onError: promise.fail)
+            }.output { packet in
+                guard packet.payload.first == 0x00 else {
+                    promise.fail(MySQLError(packet: packet))
+                    return
+                }
+                
+                promise.complete()
+            }.catch(onError: promise.fail)
         
         self.serializer.queue(Packet(data: data))
         
