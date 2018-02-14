3bb63436405a8253597037fe6d57f13d10d951ac
Joannis Orlandos <joannis@orlandos.nl>
2018-02-14 09:00:30 +0100

    Update MySQL to use exact rather than the (broken) revision for dependency versions

--- Package.swift
+++ Package.swift
@@ -8,19 +8,19 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .revision("1.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", .revision("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/core.git", .exact("3.0.0-beta.1")),
 
         // Cryptography modules
-        .package(url: "https://github.com/vapor/crypto.git", .revision("3.0.0-beta.1")),
+        .package(url: "https://github.com/vapor/crypto.git", .exact("3.0.0-beta.1")),
         
         // Networking
-        .package(url: "https://github.com/vapor/sockets.git", .revision("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/sockets.git", .exact("3.0.0-beta.2")),
 
         // SSL support
-        .package(url: "https://github.com/vapor/tls.git", .revision("3.0.0-beta.2")),
+        .package(url: "https://github.com/vapor/tls.git", .exact("3.0.0-beta.2")),
     ],
     targets: [
         .target(name: "MySQL", dependencies: ["CodableKit", "Crypto", "TCP", "TLS"]),
