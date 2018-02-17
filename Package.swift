850dc2a8cb70db35ba6c4c0df62e7affeea6e416
Dobi Sándor <dobi.sandor@codewizards.hu>
2018-02-17 13:42:10 +0100

    Fixing dependecies

--- Package.swift
+++ Package.swift
@@ -8,19 +8,19 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-beta.1"),
+        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-beta.1"),
+        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
 
         // Cryptography modules
-        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-beta.1"),
+        .package(url: "https://github.com/vapor/crypto.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
 
         // Networking
-        .package(url: "https://github.com/vapor/sockets.git", from: "3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/sockets.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
 
         // SSL support
-        .package(url: "https://github.com/vapor/tls.git", from:"3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/tls.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
     ],
     targets: [
         .target(name: "MySQL", dependencies: ["CodableKit", "Crypto", "TCP", "TLS"]),
