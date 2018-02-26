cd80235054f3474581669be086cd29edfb3fff9d
Joannis Orlandos <joannis@orlandos.nl>
2018-02-26 11:06:12 +0100

    Update to RC dependencies

--- Package.swift
+++ Package.swift
@@ -8,19 +8,19 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
-        .package(url: "https://github.com/vapor/core.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
 
         // Cryptography modules
-        .package(url: "https://github.com/vapor/crypto.git", "3.0.0-beta.1"..<"3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0-rc"),
 
         // Networking
-        .package(url: "https://github.com/vapor/sockets.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/sockets.git", from: "3.0.0-rc"),
 
         // SSL support
-        .package(url: "https://github.com/vapor/tls.git", "3.0.0-beta.2"..<"3.0.0-beta.3"),
+        .package(url: "https://github.com/vapor/tls.git", from: "3.0.0-rc"),
     ],
     targets: [
         .target(name: "MySQL", dependencies: ["CodableKit", "Crypto", "TCP", "TLS"]),
