576683a1266327467cdab136ad99784e8fa02f03
tanner0101 <me@tanner.xyz>
2018-03-22 15:43:00 -0400

    add extendable support

--- Sources/MySQL/Connection/MySQLConnection.swift
+++ Sources/MySQL/Connection/MySQLConnection.swift
@@ -1,8 +1,9 @@
 import Async
+import Core
 import Crypto
 import DatabaseKit
 import NIO
-import Foundation
+import Service
 
 /// A MySQL frontend client.
 public final class MySQLConnection: BasicWorker, DatabaseConnection {
@@ -23,11 +24,15 @@ public final class MySQLConnection: BasicWorker, DatabaseConnection {
     /// The current query running, if one exists.
     private var pipeline: Future<Void>
 
+    /// See `Extendable.extend`
+    public var extend: Extend
+
     /// Creates a new MySQL client with the provided MySQL packet queue and channel.
     init(queue: QueueHandler<MySQLPacket, MySQLPacket>, channel: Channel) {
         self.queue = queue
         self.channel = channel
         self.pipeline = Future.map(on: channel.eventLoop) { }
+        self.extend = [:]
     }
 
     /// Sends `MySQLPacket` to the server.
