c5d837dfc33330ff7b5ec1b46c53516f2d0f54f7
tanner0101 <me@tanner.xyz>
2018-03-27 12:14:44 -0400

    fix typo psql

--- Sources/MySQL/Database/MySQLDatabaseConfig.swift
+++ Sources/MySQL/Database/MySQLDatabaseConfig.swift
@@ -20,7 +20,7 @@ public struct MySQLDatabaseConfig {
     /// Database name.
     public let database: String
 
-    /// Creates a new `PostgreSQLDatabaseConfig`.
+    /// Creates a new `MySQLDatabaseConfig`.
     public init(hostname: String, port: Int = 3306, username: String, password: String? = nil, database: String) {
         self.hostname = hostname
         self.port = port
