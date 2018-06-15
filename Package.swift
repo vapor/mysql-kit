ce6f88ae3f1c55361363a8b2a71bc2408f014686
tanner0101 <me@tanner.xyz>
2018-06-14 23:28:06 -0400

    update to official deps

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     ],
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", .branch("fluent-gm")),
+        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
         
         // 🔑 Hashing (BCrypt, SHA, HMAC, etc), encryption, and randomness.
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
 
         // 🗄 Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", .branch("fluent-gm")),
+        .package(url: "https://github.com/vapor/database-kit.git", from: "1.0.0"),
 
         // 📦 Dependency injection / inversion of control framework.
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
