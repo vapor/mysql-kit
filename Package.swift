8cf46e39e190b1c40f08614900ea097ee0d420e5
tanner0101 <me@tanner.xyz>
2018-06-19 18:50:32 -0400

    use tags

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
 
         // 🗄 Core services for creating database integrations.
-        .package(url: "https://github.com/vapor/database-kit.git", .branch("sql")),
+        .package(url: "https://github.com/vapor/database-kit.git", from: "1.2.0"),
 
         // 📦 Dependency injection / inversion of control framework.
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0"),
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "1.0.0"),
         
         // *️⃣ Build SQL queries in Swift. Extensible, protocol-based design that supports DQL, DML, and DDL.
-        .package(url: "https://github.com/vapor/sql.git", .branch("sql")),
+        .package(url: "https://github.com/vapor/sql.git", from: "2.0.0-beta"),
 
     ],
     targets: [
