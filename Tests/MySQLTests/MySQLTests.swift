b02ab66b444d75be45541e5a633d245d2355acef
Ben Koska <ben@koska.at>
2018-07-06 00:47:57 +0200

    Added tests for MySQLTime and fixed microsecond calculation for MySQLTime

--- Tests/MySQLTests/MySQLTests.swift
+++ Tests/MySQLTests/MySQLTests.swift
@@ -157,6 +157,14 @@ class MySQLTests: XCTestCase {
             accuracy: 5
         )
     }
+ 
+    func testDateBefore1970() throws {
+        let time = Date(timeIntervalSince1970: 1.1).convertToMySQLTime()
+        let time2 = Date(timeIntervalSince1970: -1.1).convertToMySQLTime()
+        
+        XCTAssert(time.microsecond == UInt32(100000))
+        XCTAssert(time2.microsecond == UInt32(100000))
+    }
 
     func testSaveEmoticonsUnicode() throws {
         let client = try MySQLConnection.makeTest()
