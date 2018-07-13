499a317ab645062f34955bd79f0991fb1c1a0ad8
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-07-13 07:40:10 +0200

    Improve decoding from data

--- Sources/MySQL/Connection/MySQLGeometry.swift
+++ Sources/MySQL/Connection/MySQLGeometry.swift
@@ -1,3 +1,6 @@
+import NIO
+
+/// This structure is used to represent GEOMETRY data (Currently only Points).
 public enum MySQLGeometry: Equatable {
     case point(x: Double, y: Double)
 
@@ -11,6 +14,7 @@ public enum MySQLGeometry: Equatable {
 }
 
 extension MySQLGeometry: CustomStringConvertible {
+    /// See `CustomStringConvertible`.
     public var description: String {
         switch self {
         case .point(x: let x, y: let y):
@@ -23,7 +27,7 @@ extension MySQLGeometry {
     func convertToData() -> Data {
         switch self {
         case .point(let x, let y):
-            let bufferSize = MySQLGeometry.headerSize + 2 * MemoryLayout<Double>.size
+            let bufferSize = MySQLGeometry.headerSize + 16 // 2 `Double`s
             var buffer = ByteBufferAllocator().buffer(capacity: bufferSize)
             buffer.write(integer: UInt32(0)) // 4 byte filler
             buffer.write(integer: UInt8(1)) // set little endian byte order
@@ -43,12 +47,13 @@ extension MySQLGeometry {
             throw MySQLError(identifier: "geometryType", reason: "Not enough data to read geometry type.")
         }
         var buffer = ByteBufferAllocator().buffer(capacity: count)
-        buffer.withUnsafeMutableWritableBytes { (p) in
-            p.copyBytes(from: data)
+        buffer.write(bytes: data)
+
+        _ = try buffer.requireInteger(endianness: .little, as: UInt32.self)
+        guard try buffer.requireInteger(endianness: .little, as: UInt8.self) == 1 else {
+            throw MySQLError(identifier: "endianness", reason: "Expected value of `1` for endianness.")
         }
 
-        let _: UInt32 = try buffer.requireInteger()
-        let _: UInt8 = try buffer.requireInteger()
         let wkbType: UInt32 = try buffer.requireInteger(endianness: .little)
 
         switch wkbType {
@@ -63,13 +68,15 @@ extension MySQLGeometry {
     }
 }
 
-extension MySQLGeometry: ReflectionDecodable {
-    public static func reflectDecoded() throws -> (MySQLGeometry, MySQLGeometry) {
-        return (.point(x: 0, y: 0), .point(x: 0, y: 1))
+extension MySQLGeometry: MySQLDataTypeStaticRepresentable {
+    /// See `MySQLDataTypeStaticRepresentable`.
+    public static var mysqlDataType: MySQLDataType {
+        return .geometry
     }
 }
 
 extension MySQLGeometry: MySQLDataConvertible {
+    /// See `MySQLDataConvertible`.
     public func convertToMySQLData() -> MySQLData {
         let binary = MySQLBinaryData(
             type: .MYSQL_TYPE_GEOMETRY,
@@ -79,6 +86,7 @@ extension MySQLGeometry: MySQLDataConvertible {
         return MySQLData(storage: .binary(binary))
     }
 
+    /// See `MySQLDataConvertible`.
     public static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> MySQLGeometry {
         switch mysqlData.storage {
         case .binary(let binary):
