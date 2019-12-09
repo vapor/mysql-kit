6c43e6d9e7a1177df24a790ca0ffabd47ffd4c2e
tanner0101 <me@tanner.xyz>
2019-12-09 15:10:47 -0500

    beta.2

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "mysql-kit",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     products: [
         .library(name: "MySQLKit", targets: ["MySQLKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/mysql-nio.git", .branch("master")),
-        .package(url: "https://github.com/vapor/sql-kit.git", .branch("master")),
-        .package(url: "https://github.com/vapor/async-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-beta.2"),
 
     ],
     targets: [
