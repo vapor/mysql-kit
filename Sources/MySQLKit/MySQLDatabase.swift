247ed00f1c264891b05167146ca41a5c13381f70
Tanner <tannernelson@gmail.com>
2019-12-13 17:04:49 -0500

    sql row protocol updates (#251)

--- Sources/MySQLKit/MySQLDatabase.swift
+++ Sources/MySQLKit/MySQLDatabase.swift
@@ -91,10 +91,26 @@ public struct MySQLConnectionSource: ConnectionPoolSource {
 
 extension MySQLConnection: ConnectionPoolItem { }
 
+struct MissingColumn: Error {
+    let column: String
+}
+
 extension MySQLRow: SQLRow {
+    public var allColumns: [String] {
+        self.columnDefinitions.map { $0.name }
+    }
+
+    public func contains(column: String) -> Bool {
+        self.columnDefinitions.contains { $0.name == column }
+    }
+
+    public func decodeNil(column: String) throws -> Bool {
+        self.column(column) == nil
+    }
+
     public func decode<D>(column: String, as type: D.Type) throws -> D where D : Decodable {
         guard let data = self.column(column) else {
-            fatalError()
+            throw MissingColumn(column: column)
         }
         return try MySQLDataDecoder().decode(D.self, from: data)
     }
