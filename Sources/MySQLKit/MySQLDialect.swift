f690cac2bdd70ece778b655acb8e11d06bee473b
Scott Grosch <Scott.Grosch@icloud.com>
2020-01-23 22:10:07 -0800

    Added create/drop trigger dialect conditions

---
+++ Sources/MySQLKit/MySQLDialect.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public struct MySQLDialect: SQLDialect {
+    public init() {}
+
+    public var name: String {
+        "mysql"
+    }
+
+    public var identifierQuote: SQLExpression {
+        return SQLRaw("`")
+    }
+
+    public func bindPlaceholder(at position: Int) -> SQLExpression {
+        return SQLRaw("?")
+    }
+
+    public func literalBoolean(_ value: Bool) -> SQLExpression {
+        switch value {
+        case false:
+            return SQLRaw("0")
+        case true:
+            return SQLRaw("1")
+        }
+    }
+
+    public var autoIncrementClause: SQLExpression {
+        return SQLRaw("AUTO_INCREMENT")
+    }
+
+    public var supportsAutoIncrement: Bool {
+        true
+    }
+
+    public var enumSyntax: SQLEnumSyntax {
+        .inline
+    }
+
+    public var createTriggerSupportsBody = true
+    public var createTriggerConditionRequiresParens = true
+    public var createTriggerSupportsOrder = true
+}
