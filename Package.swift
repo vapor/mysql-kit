2d46f14ea2cd79b5013d4d7d93f9ab4cee15207e
Tanner <tannernelson@gmail.com>
2020-02-28 16:36:46 -0500

    Release Candidate 1 (#262)
    * rc.1
    
    * fix package decls
    
    * rc.1
    
    * updates
    
    * 5.2
    
    * test latest

--- Package.swift
+++ Package.swift
@@ -1,22 +1,29 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "mysql-kit",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "MySQLKit", targets: ["MySQLKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-rc.1"),
 
     ],
     targets: [
-        .target(name: "MySQLKit", dependencies: ["AsyncKit", "MySQLNIO", "SQLKit"]),
-        .testTarget(name: "MySQLKitTests", dependencies: ["MySQLKit", "SQLKitBenchmark"]),
+        .target(name: "MySQLKit", dependencies: [
+            .product(name: "AsyncKit", package: "async-kit"),
+            .product(name: "MySQLNIO", package: "mysql-nio"),
+            .product(name: "SQLKit", package: "sql-kit"),
+        ]),
+        .testTarget(name: "MySQLKitTests", dependencies: [
+            .target(name: "MySQLKit"),
+            .product(name: "SQLKitBenchmark", package: "sql-kit"),
+        ]),
     ]
 )
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
     <img src="https://user-images.githubusercontent.com/1342803/36769061-0a96b82c-1c10-11e8-8c66-4b7a337bbb43.png" height="64" alt="MySQL">
     <br>
     <br>
-    <a href="https://docs.vapor.codes/3.0/">
+    <a href="https://docs.vapor.codes/4.0/">
         <img src="http://img.shields.io/badge/read_the-docs-2196f3.svg" alt="Documentation">
     </a>
     <a href="https://discord.gg/vapor">
@@ -11,10 +11,10 @@
     <a href="LICENSE">
         <img src="http://img.shields.io/badge/license-MIT-brightgreen.svg" alt="MIT License">
     </a>
-    <a href="https://circleci.com/gh/vapor/mysql">
-        <img src="https://circleci.com/gh/vapor/mysql-kit.svg?style=shield" alt="Continuous Integration">
+    <a href="https://github.com/vapor/sql-kit/actions">
+        <img src="https://github.com/vapor/sql-kit/workflows/test/badge.svg" alt="Continuous Integration">
     </a>
     <a href="https://swift.org">
-        <img src="http://img.shields.io/badge/swift-5-brightgreen.svg" alt="Swift 5">
+        <img src="http://img.shields.io/badge/swift-5.2-brightgreen.svg" alt="Swift 5.2">
     </a>
 </p>
