f5e535844bf9b7181cf3774f838d3df724314ba4
tanner0101 <me@tanner.xyz>
2020-02-28 13:08:29 -0500

    rc.1

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "MySQLKit", targets: ["MySQLKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc.1"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0-rc.1"),
 
     ],
