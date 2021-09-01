529bde8ce86c8a2546e7e8856c206f3358dff41e
Gwynne Raskind <gwynne@darkrainfall.org>
2021-09-01 13:08:52 -0500

    Address issues in decoding data from the database (#298)
    * Retool MySQLDataDecoder to use a safe and well-supported code path for falling back on JSON decoding if all else fails. This has the side effect of making JSON decoding nested in a single-value container correctly use any custom JSON decoder, and reuses the existing MySQLDataDecoder as needed. The conversion of ByteBuffers to Foundation Data objects is also better handled (as a side effect, an explicit dependency on SwiftNIO is now declared). A minor performance improvement of up to ~3% has been observed for decoding of some models depending on the types of their properties.
    
    * Update usage of TLSConfiguration to fix deprecation warnings from NIOSSL. Add an explicit dependency on the minimum NIOSSL version which is now required as a result.
    
    * Basic CI updates (not really complete but at least it's a start)

--- Package.swift
+++ Package.swift
@@ -14,6 +14,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
     ],
     targets: [
         .target(name: "MySQLKit", dependencies: [
@@ -21,6 +23,8 @@ let package = Package(
             .product(name: "MySQLNIO", package: "mysql-nio"),
             .product(name: "SQLKit", package: "sql-kit"),
             .product(name: "Crypto", package: "swift-crypto"),
+            .product(name: "NIOFoundationCompat", package: "swift-nio"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl"),
         ]),
         .testTarget(name: "MySQLKitTests", dependencies: [
             .target(name: "MySQLKit"),
