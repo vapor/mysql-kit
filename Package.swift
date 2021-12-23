c5f39b81d5a46dd02c559721b6457f7f49f0200d
Gwynne Raskind <gwynne@darkrainfall.org>
2021-12-22 19:46:22 -0600

    Use the recommended updated SwiftCrypto version pin so users of Fluent with MySQL don't get stuck on 1.1.6.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/mysql-nio.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.16.0"),
         .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.14.0"),
     ],
