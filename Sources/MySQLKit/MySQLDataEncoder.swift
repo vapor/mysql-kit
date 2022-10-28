7d7e6e362d9147fceb13b3f5415772f7fb97de17
Gwynne Raskind <gwynne@vapor.codes>
2022-10-28 11:25:40 -0500

    Revise MySQLDataEncoder implementation to better handle various Codable conformances (#313)
    Revise MySQLDataEncoder implementation to better handle various Codable conformances (no more corrupted data when superEncoder() is used)

--- Sources/MySQLKit/MySQLDataEncoder.swift
+++ Sources/MySQLKit/MySQLDataEncoder.swift
@@ -11,13 +11,26 @@ public struct MySQLDataEncoder {
         if let custom = value as? MySQLDataConvertible, let data = custom.mysqlData {
             return data
         } else {
-            let encoder = _Encoder()
-            try value.encode(to: encoder)
-            if let data = encoder.data {
-                return data
-            } else {
+            let encoder = _Encoder(parent: self)
+            do {
+                try value.encode(to: encoder)
+                if let value = encoder.value {
+                    return value
+                } else {
+                    throw _Encoder.NonScalarValueSentinel()
+                }
+            } catch is _Encoder.NonScalarValueSentinel {
                 var buffer = ByteBufferAllocator().buffer(capacity: 0)
-                try buffer.writeBytes(self.json.encode(_Wrapper(value)))
+#if swift(<5.7)
+                struct _Wrapper: Encodable {
+                    let encodable: Encodable
+                    init(_ encodable: Encodable) { self.encodable = encodable }
+                    func encode(to encoder: Encoder) throws { try self.encodable.encode(to: encoder) }
+                }
+                try buffer.writeBytes(self.json.encode(_Wrapper(value))) // Swift <5.7 will complain that "Encodable does not conform to Encodable" without the wrapper
+#else
+                try buffer.writeBytes(self.json.encode(value))
+#endif
                 return MySQLData(
                     type: .string,
                     format: .text,
@@ -27,145 +40,50 @@ public struct MySQLDataEncoder {
             }
         }
     }
-    
+
     private final class _Encoder: Encoder {
-        var codingPath: [CodingKey] {
-            return []
-        }
-        
-        var userInfo: [CodingUserInfoKey : Any] {
-            return [:]
-        }
-        var data: MySQLData?
-        init() {
+        struct NonScalarValueSentinel: Error {}
 
-        }
+        var userInfo: [CodingUserInfoKey : Any] { [:] }; var codingPath: [CodingKey] { [] }
+        var parent: MySQLDataEncoder, value: MySQLData?
         
-        func container<Key>(keyedBy type: Key.Type) -> KeyedEncodingContainer<Key> where Key : CodingKey {
-            return .init(_KeyedValueEncoder(self))
-        }
-        
-        func unkeyedContainer() -> UnkeyedEncodingContainer {
-            _UnkeyedEncoder(self)
-        }
-        
+        init(parent: MySQLDataEncoder) { self.parent = parent }
+        func container<K: CodingKey>(keyedBy: K.Type) -> KeyedEncodingContainer<K> { .init(_FailingKeyedContainer()) }
+        func unkeyedContainer() -> UnkeyedEncodingContainer { _TaintedEncoder() }
         func singleValueContainer() -> SingleValueEncodingContainer {
-            _SingleValueEncoder(self)
-        }
-    }
-    
-    struct DoJSON: Error {}
-
-    private struct _UnkeyedEncoder: UnkeyedEncodingContainer {
-        var codingPath: [CodingKey] {
-            self.encoder.codingPath
-        }
-        var count: Int {
-            0
-        }
-
-        let encoder: _Encoder
-        init(_ encoder: _Encoder) {
-            self.encoder = encoder
-        }
-
-
-        mutating func encodeNil() throws { }
-
-        mutating func encode<T>(_ value: T) throws
-            where T : Encodable
-        { }
-
-        mutating func nestedContainer<NestedKey>(
-            keyedBy keyType: NestedKey.Type
-        ) -> KeyedEncodingContainer<NestedKey>
-            where NestedKey : CodingKey
-        {
-            self.encoder.container(keyedBy: NestedKey.self)
-        }
-
-        mutating func nestedUnkeyedContainer() -> UnkeyedEncodingContainer {
-            self.encoder.unkeyedContainer()
-        }
-
-        mutating func superEncoder() -> Encoder {
-            self.encoder
-        }
-    }
-    
-    private struct _KeyedValueEncoder<Key>: KeyedEncodingContainerProtocol where Key: CodingKey {
-        var codingPath: [CodingKey] {
-            return self.encoder.codingPath
+            precondition(self.value == nil, "Requested multiple containers from the same encoder.")
+            return _SingleValueContainer(encoder: self)
         }
         
-        let encoder: _Encoder
-        init(_ encoder: _Encoder) {
-            self.encoder = encoder
+        struct _SingleValueContainer: SingleValueEncodingContainer {
+            let encoder: _Encoder; var codingPath: [CodingKey] { self.encoder.codingPath }
+            func encodeNil() throws { self.encoder.value = .null }
+            func encode<T: Encodable>(_ value: T) throws { self.encoder.value = try self.encoder.parent.encode(value) }
         }
         
-        mutating func encodeNil(forKey key: Key) throws { }
-        
-        mutating func encode<T>(_ value: T, forKey key: Key) throws
-            where T : Encodable
-        { }
-        
-        mutating func nestedContainer<NestedKey>(
-            keyedBy keyType: NestedKey.Type,
-            forKey key: Key
-        ) -> KeyedEncodingContainer<NestedKey> where NestedKey : CodingKey {
-            self.encoder.container(keyedBy: NestedKey.self)
+        /// This pair of types is only necessary because we can't directly throw an error from various Encoder and
+        /// encoding container methods. We define duplicate types rather than the old implementation's use of a
+        /// no-action keyed container because it can save a significant amount of time otherwise spent uselessly calling
+        /// nested methods in some cases.
+        struct _TaintedEncoder: Encoder, UnkeyedEncodingContainer, SingleValueEncodingContainer {
+            var userInfo: [CodingUserInfoKey : Any] { [:] }; var codingPath: [CodingKey] { [] }; var count: Int { 0 }
+            func container<K: CodingKey>(keyedBy: K.Type) -> KeyedEncodingContainer<K> { .init(_FailingKeyedContainer()) }
+            func nestedContainer<K: CodingKey>(keyedBy: K.Type) -> KeyedEncodingContainer<K> { .init(_FailingKeyedContainer()) }
+            func unkeyedContainer() -> UnkeyedEncodingContainer { self }
+            func nestedUnkeyedContainer() -> UnkeyedEncodingContainer { self }
+            func singleValueContainer() -> SingleValueEncodingContainer { self }
+            func superEncoder() -> Encoder { self }
+            func encodeNil() throws { throw NonScalarValueSentinel() }
+            func encode<T: Encodable>(_: T) throws { throw NonScalarValueSentinel() }
         }
-        
-        mutating func nestedUnkeyedContainer(forKey key: Key) -> UnkeyedEncodingContainer {
-            self.encoder.unkeyedContainer()
+        struct _FailingKeyedContainer<K: CodingKey>: KeyedEncodingContainerProtocol {
+            var codingPath: [CodingKey] { [] }
+            func encodeNil(forKey: K) throws { throw NonScalarValueSentinel() }
+            func encode<T: Encodable>(_: T, forKey: K) throws { throw NonScalarValueSentinel() }
+            func nestedContainer<NK: CodingKey>(keyedBy: NK.Type, forKey: K) -> KeyedEncodingContainer<NK> { .init(_FailingKeyedContainer<NK>()) }
+            func nestedUnkeyedContainer(forKey: K) -> UnkeyedEncodingContainer { _TaintedEncoder() }
+            func superEncoder() -> Encoder { _TaintedEncoder() }
+            func superEncoder(forKey: K) -> Encoder { _TaintedEncoder() }
         }
-        
-        mutating func superEncoder() -> Encoder {
-            self.encoder
-        }
-        
-        mutating func superEncoder(forKey key: Key) -> Encoder {
-            self.encoder
-        }
-    }
-    
-    
-    private struct _SingleValueEncoder: SingleValueEncodingContainer {
-        var codingPath: [CodingKey] {
-            return self.encoder.codingPath
-        }
-        
-        let encoder: _Encoder
-        init(_ encoder: _Encoder) {
-            self.encoder = encoder
-        }
-        
-        mutating func encodeNil() throws {
-            self.encoder.data = MySQLData.null
-        }
-        
-        mutating func encode<T>(_ value: T) throws where T : Encodable {
-            if let convertible = value as? MySQLDataConvertible {
-                guard let data = convertible.mysqlData else {
-                    throw EncodingError.invalidValue(convertible, EncodingError.Context(
-                        codingPath: self.codingPath,
-                        debugDescription: "Could not encode \(T.self) to MySQL data: \(value)"
-                    ))
-                }
-                self.encoder.data = data
-            } else {
-                try value.encode(to: self.encoder)
-            }
-        }
-    }
-}
-
-struct _Wrapper: Encodable {
-    let encodable: Encodable
-    init(_ encodable: Encodable) {
-        self.encodable = encodable
-    }
-    func encode(to encoder: Encoder) throws {
-        try self.encodable.encode(to: encoder)
     }
 }
