d944fb3289b8341ffbdf157c504db1f494e36ed0
Gwynne Raskind <gwynne@vapor.codes>
2023-04-13 20:00:38 -0500

    Update min Swift version to 5.6 and make platform versions consistent (#320)
    Update minimum Swift version to 5.6 and make platform versions consistent.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "mysql-kit",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
     ],
     products: [
         .library(name: "MySQLKit", targets: ["MySQLKit"]),
