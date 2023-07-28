5d1b1a67766c4684d5c6c3bcbc323438aefb307d
Gwynne Raskind <gwynne@vapor.codes>
2023-07-27 23:23:54 -0500

    Add CODEOWNERS, fixup Exports.swift

--- Sources/MySQLKit/Exports.swift
+++ Sources/MySQLKit/Exports.swift
@@ -1,4 +1,13 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import MySQLNIO
+@_documentation(visibility: internal) @_exported import AsyncKit
+@_documentation(visibility: internal) @_exported import SQLKit
+@_documentation(visibility: internal) @_exported import struct Foundation.URL
+@_documentation(visibility: internal) @_exported import struct Foundation.Data
+@_documentation(visibility: internal) @_exported import struct NIOSSL.TLSConfiguration
+
+#else
 
 @_exported import MySQLNIO
 @_exported import AsyncKit
